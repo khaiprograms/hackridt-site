077e01cb86dab1b01ac86f4632201aafff917847
Aaron Lam <aaronlammpn@gmail.com>
2022-10-15 12:38:33 +0800

    Profile creation with image

--- hackathonSite/hackathonSite/urls.py
+++ hackathonSite/hackathonSite/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from accounts import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("profile/", user_views.profile, name="profile"),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
